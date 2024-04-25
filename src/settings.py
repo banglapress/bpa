a3e1fc4e31269a975d2fba4333b6ae09ccef755d
banglapress <zafar21@gmail.com>
2024-04-26 01:09:04 +0600

    hosting on vercel.com

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ho$hx#$r&66*)r%3#o^3n3h*k0+)na25oy5rh)%yempb!*!)t!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -81,11 +81,18 @@ WSGI_APPLICATION = "src.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'oPiUAxJcaUurTXgIpBnnTKGcuedMzSkt',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '44144',
     }
 }
+
+
+
 
 
 # Password validation
@@ -134,6 +141,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
